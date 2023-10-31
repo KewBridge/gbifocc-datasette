424fd94c5bbb08c9a983fc3c6d691e54b3f875e1
Nicky Nicolson <nicky.nicolson@gmail.com>
2023-10-31 15:51:04 +0000

    MOved create cols into format conv script

--- tab2csv.py
+++ tab2csv.py
@@ -1,9 +1,13 @@
 import argparse
 import pandas as pd
+import bananompy
+from tqdm import tqdm
+tqdm.pandas()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("inputfile")
+    parser.add_argument("-createcols", action='store_true')
     parser.add_argument("outputfile")    
     args = parser.parse_args()
 
@@ -12,4 +16,13 @@ if __name__ == '__main__':
                     keep_default_na=False, 
                     on_bad_lines='skip', 
                     sep='\t')
+    if args.createcols:
+        # Extract unique recordedBy values
+        df_rb = df[['recordedBy']].drop_duplicates()
+        df_rb['recordedBy_first_familyname'] = df_rb.recordedBy.progress_apply(getFirstFamilyName)
+        # Apply back to main dataframe
+        df = pd.merge(left = df, right=df_rb, left_on='recordedBy', right_on='recordedBy', how='left')
+        # Add column holding collector name and number
+        mask = (df.recordNumber.notnull())
+        df.loc[mask,'collectorNameAndNumber']=df[mask].apply(lambda row: '{} {}'.format(row['recordedBy_first_familyname'],row['recordNumber']),axis=1)
     df.to_csv(args.outputfile, index=False, sep=',')    
