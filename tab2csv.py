e57384a19ec7e66f6a402dd0dd4148b9c0247a06
Nicky Nicolson <nicky.nicolson@gmail.com>
2023-10-31 14:44:06 +0000

    skip bad lines

--- tab2csv.py
+++ tab2csv.py
@@ -7,5 +7,9 @@ if __name__ == '__main__':
     parser.add_argument("outputfile")    
     args = parser.parse_args()
 
-    df = pd.read_csv(args.inputfile, encoding='utf8', keep_default_na=False, sep='\t')
+    df = pd.read_csv(args.inputfile, 
+                    encoding='utf8', 
+                    keep_default_na=False, 
+                    on_bad_lines='skip', 
+                    sep='\t')
     df.to_csv(outputfile, index=False, sep=',')    
