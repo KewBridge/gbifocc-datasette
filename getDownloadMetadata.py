4d96b325b072ad8701f34b05a57418c3d92bd854
Nicky Nicolson <nicky.nicolson@gmail.com>
2023-11-03 13:55:34 +0000

    Added licence name

--- getDownloadMetadata.py
+++ getDownloadMetadata.py
@@ -2,6 +2,8 @@ import argparse
 from pygbif import occurrences as occ
 import json
 
+
+licences = {'http://creativecommons.org/licenses/by-nc/4.0/legalcode':'CC BY-NC 4.0'}
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("inputfile")
@@ -15,9 +17,12 @@ if __name__ == '__main__':
         datasette_metadata = json.load(f_in)
  
     gbif_metadata = occ.download_meta(key = args.download_id)
-    datasette_metadata['licence'] = gbif_metadata['license']
-    datasette_metadata['source_url'] = 'https://doi.org{}'.format(gbif_metadata['doi'])
+    licence_url = gbif_metadata['license']
+    if licence_url in licences:
+        datasette_metadata['licence'] = licences[licence_url]
+        datasette_metadata['licence_url'] = licence_url
+    datasette_metadata['source_url'] = 'https://doi.org/{}'.format(gbif_metadata['doi'])
 
-    datasette_metadata_json = json.dumps(datasette_metadata)
+    datasette_metadata_json = json.dumps(datasette_metadata, indent=4)
     with open(args.outputfile, 'w') as f_out:
         f_out.write(datasette_metadata_json)
